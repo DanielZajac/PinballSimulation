fdb19740fd287d0b0083dd59e550654e7e175b73
DanielZajac <daniel.zajac@ontariotechu.net>
2024-04-04 00:02:45 -0400

    Collision detection function implemented for point mass

--- pinball.py
+++ pinball.py
@@ -4,12 +4,71 @@ author 2s: Heisn Nithysingha - 100817036
 """
 
 import pygame
+import math
 import sys
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.integrate import ode
 import random
 from datetime import datetime
+
+#we wil store all of our shapes, and each shape will be a list of vertices (each with an x and y)
+shapes = []
+
+#checks if there is a collision, and returns the line which was collided with
+def iscollision(point1, point2):
+    point_line_slope = (point2[1]-point1[1])/(point2[0]-point1[0]) if point2[0] - point1[0] != 0 else float('inf')
+
+    potential_lines = []
+    for shape in shapes:
+        for i in range(len(shape)-1):
+            shape_line_slope = (shape[i][1]-shape[i+1][1])/(shape[i][0]-shape[i+1][0]) if shape[i][0] - shape[i+1][0] != 0 else float('inf')
+            if point_line_slope == float('inf'): #point line is vertical
+                x_intersect = point1[0]
+                y_intersect = shape_line_slope * (x_intersect - shape[i][0]) + shape[i][1]
+            elif shape_line_slope == float('inf'): #line between shape vertices is vertical
+                x_intersect = shape[i][0]
+                y_intersect = point_line_slope * (x_intersect - point1[0]) + point1[1]
+            else:
+                x_intersect = (point_line_slope * point1[0] - shape_line_slope * shape[i][0] + shape[i][1] - point1[1]) / (point_line_slope - shape_line_slope)
+                y_intersect = point_line_slope * (x_intersect - point1[0]) + point1[1]
+            
+            #checking if the intersection point is within the acutal line, not far off in the distance
+            if (x_intersect >= min(point1[0], point2[0]) and x_intersect <= max(point1[0], point2[0]) and
+                y_intersect >= min(point1[1], point2[1]) and y_intersect <= max(point1[1], point2[1]) and
+                x_intersect >= min(shape[i][0], shape[i+1][0]) and x_intersect <= max(shape[i][0], shape[i+1][0]) and
+                y_intersect >= min(shape[i][1], shape[i+1][1]) and y_intersect <= max(shape[i][1], shape[i+1][1])):
+                potential_lines.append(shape[i], shape[i+1]) #adding the two vertices line to the list of potential first contacts
+                
+    #now we have a list of the collision lines which are intersected, and we want to find which line was collided with first
+    if len(potential_lines) != 0:
+        min_distance_to_line = 10000000000
+        for line in potential_lines:
+            #finding the distance from ball position to collision line
+            a = line[1][1] - line[0][1]
+            b = line[0][0] - line[1][0]
+            c = line[1][0]*line[0][1] - line[0][0]*line[1][1]
+            distance_to_line = abs(a*point1[0] + b*point1[1] + c) / math.sqrt(a**2 + b**2)
+
+            #want to find what the ball collided with first
+            if distance_to_line < min_distance_to_line:
+                closest_line = line
+                min_distance_to_line = distance_to_line
+
+        return True, closest_line
+    #if there are no lines collided with, return false, and a filler value
+    else:
+        return False, [[0, 0], [0, 0]]
+
+
+
+
+
+
+
+
+
+
 
 # set up the colors
 BLACK = (0, 0, 0)
