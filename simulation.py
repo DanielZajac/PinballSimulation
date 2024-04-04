4027c689b092956b53eb89712a82467632870895
Heisn Nithysingha <heisn.nithy@gmail.com>
2024-04-04 16:35:14 -0400

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -3,6 +3,16 @@
 # Import and initialize the pygame library
 import pygame
 import pinball
+import numpy as np
+import math
+
+def rotated_points(point, angle, r_point):
+    Org_point = (point[0] - r_point[0], point[1] - r_point[1]) #
+    x_ = (Org_point[0]* math.cos(math.radians(angle))) - (Org_point[1]* math.cos(math.radians(angle)))
+    y_ = (Org_point[0]* math.sin(math.radians(angle))) + (Org_point[1]* math.cos(math.radians(angle)))
+    print(f'Mouse clicked at ({x_ + r_point[0]}, { y_ + r_point[1]})')
+    return ((x_ + r_point[0], y_ + r_point[1]))
+    
 
 from pygame.locals import (
     K_LEFT,
@@ -24,6 +34,7 @@ PINB_RIGHT = 0
 PINB_TOP = 50
 PINB_BOTTOM = 0
 
+L_ANGLE = 0
 # Create the screen object
 # The size is determined by the constant SCREEN_WIDTH and SCREEN_HEIGHT
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -58,6 +69,16 @@ while running:
     pygame.draw.polygon(screen, (255,0,0), ((35, 50),(35, 150),(145, 50))) #top left triangle
     pygame.draw.polygon(screen, (255,0,0), ((35, 950),(35, 770),(255, 950))) #bottom left triangle
     pygame.draw.polygon(screen, (255,0,0), ((730, 950),(730, 770),(510, 950))) #bottom right triangle
+    l_rotated_point = (201, 906)
+    pygame.draw.polygon(screen, (0,0,0), (rotated_points((200, 930), L_ANGLE, l_rotated_point), rotated_points((200, 890), L_ANGLE, l_rotated_point), rotated_points((320, 910), L_ANGLE, l_rotated_point))) #left bumper
+    pygame.draw.polygon(screen, (0,0,0), ((550, 890),(550, 930),(435, 910))) #right bumper
+    
+    l_x_offset = 10
+    l_y_offset = 20
+    pygame.draw.polygon(screen, (0,255,0), ((164, 243),(140, 264), (190, 310), (241, 264), (217, 243))) #diamond bumper
+    pygame.draw.polygon(screen, (0,255,0), (rotated_points((530 + l_x_offset, 345 + l_y_offset), 45, (530, 345)), rotated_points((570+ l_x_offset, 345+ l_y_offset), 45, (530, 345)),  rotated_points((570+ l_x_offset, 515+ l_y_offset), 45, (530, 345)),  rotated_points((670+ l_x_offset, 515+ l_y_offset), 45, (530, 345)),  rotated_points((670+ l_x_offset, 545+ l_y_offset), 45, (530, 345)),  rotated_points((530+ l_x_offset, 545+ l_y_offset), 45, (530, 345)),  rotated_points((530+ l_x_offset,345+ l_y_offset), 45, (530, 345)))) #l bumper
+    
+    rotated_points((530, 345), 45, (670, 545))
     
     
     
